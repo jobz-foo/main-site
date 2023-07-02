b948b9338db89dbdf8a1f738e76a803a7e29be56
Blacky Wolf <support@blackywolf.me>
2023-07-01 22:28:21 -0500

    Chanegd tab size to 4 spaces

--- src/islands/Counter.tsx
+++ src/islands/Counter.tsx
@@ -2,15 +2,15 @@ import type { Signal } from "@preact/signals";
 import { Button } from "../components/Button.tsx";
 
 interface CounterProps {
-  count: Signal<number>;
+    count: Signal<number>;
 }
 
 export default function Counter(props: CounterProps) {
-  return (
-    <div class="flex gap-2 w-full">
-      <p class="flex-grow-1 font-bold text-xl">{props.count}</p>
-      <Button onClick={() => props.count.value -= 1}>-1</Button>
-      <Button onClick={() => props.count.value += 1}>+1</Button>
-    </div>
-  );
+    return (
+        <div class="flex gap-2 w-full">
+            <p class="flex-grow-1 font-bold text-xl">{props.count}</p>
+            <Button onClick={() => props.count.value -= 1}>-1</Button>
+            <Button onClick={() => props.count.value += 1}>+1</Button>
+        </div>
+    );
 }
